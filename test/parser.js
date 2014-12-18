979719120228689f2f1dacf98dca14bf2034697b
Rob Brackett <rob@robbrackett.com>
2014-12-18 16:52:14 -0500

    A few more parser tests

--- test/parser.js
+++ test/parser.js
@@ -28,4 +28,59 @@ describe("Parser", function() {
     assert.equal(result.queries[0].rules[0].units, "em", "The query's units should be 'em'");
   });
   
+  it("skips @rules", function() {
+    var result = elementQuery.parser.parseStyleText(
+      "@import url(\"test2.css\");\
+      .test-element { background: purple; }"
+    );
+    assert.equal(result.queries.length, 0, "Should not have found any queries.");
+  });
+  
+  it("skips @rules like @font-face that have a block of properties", function() {
+    var result = elementQuery.parser.parseStyleText(
+      "@font-face {\
+        font-family: \"font of all knowledge\";\
+        src: local(\"font of all knowledge\"), local(fontofallknowledge), url(fontofallknowledge.woff);\
+        font-weight: 400;\
+        font-style: normal;\
+      }"
+    );
+    assert.equal(result.queries.length, 0, "Should not have found any queries.");
+  });
+  
+  it("finds element queries inside media queries", function() {
+    var result = elementQuery.parser.parseStyleText(
+      "@media all and (-webkit-min-device-pixel-ratio: 5) {\
+        .test-element {\
+            background: red;\
+        }\
+        .test-element:media(max-available-width: 30em) {\
+            background: yellow;\
+        }\
+      }"
+    );
+    assert.equal(result.queries.length, 1, "Should have found 1 query.");
+    console.log(result);
+  });
+  
+  it("finds element queries inside nested media queries", function() {
+    var result = elementQuery.parser.parseStyleText(
+      "@media all and (-webkit-min-device-pixel-ratio: 5) {\
+        .test-element {\
+            background: red;\
+        }\
+        .test-element:media(max-available-width: 30em) {\
+            background: yellow;\
+        }\
+        /* UHOH, nested queries! */\
+        @media all and (-webkit-min-device-pixel-ratio: 6) {\
+            .test-element:media(max-available-width: 40em) {\
+                background: rainbow;\
+            }\
+        }\
+      }"
+    );
+    assert.equal(result.queries.length, 2, "Should have found 2 queries.");
+  });
+  
 });
